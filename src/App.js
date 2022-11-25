527d5921f2c7358e5cac586bb5f4aec4ff6436a8
ESPP-ZDT <toplelfaget@gmail.com>
2022-11-25 04:37:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import Axios from 'axios'
 
 function App() {
+
+  const[name,setName] =useState("");
+  const[email,setEmail] =useState("");
+
+  const[usersList,setUsersList] = useState([])
+  
+  const addUser = () =>{
+    Axios.post('http://localhost:3001/create',
+    {name:name, email:email}).then(()=>{console.log("succes")});
+  }
+
+  const getUsers = () =>{
+    Axios.get('http://localhost:3001/users').then((
+      response
+    )=>{console.log(response)})
+
+  }
+
+  
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputDisplay">
+        <label>UserName</ label>  
+        <input type="text" 
+        onChange={(event) =>{
+          setName(event.target.value);
+        }}/>
+        <label>UserEmail</label>  
+        <input type="text"
+        onChange={(event) =>{
+          setEmail(event.target.value);
+        }}/>
+        <button onClick={addUser} >Add User</button>
+        <div className="button">
+        <button onClick={getUsers} >show users</button>
+        </div>
+      </div>
     </div>
   );
 }
